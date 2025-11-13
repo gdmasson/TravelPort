920f0ebfa6ab38efcae8f4415e135c2fcbcddc0c
Gabriel Masson <157505893+gdmasson@users.noreply.github.com>
2025-11-13 13:41:37 +0100

    add test

---
+++ TravelPort.UnitTests/PassportNumberValidation.cs
@@ -0,0 +1,30 @@
+﻿using TravelPort.Core.Services;
+
+namespace TravelPort.UnitTests;
+
+public class PassportNumberValidation
+{
+    [Theory]
+    [InlineData("PA1234567")]
+    [InlineData("LA1234567")]
+    public void PassportShouldBeValid(string passportNumber)
+    {
+        // Act
+        var isValid = UserService.ValidatePassportNumber(passportNumber);
+        
+        // Assert
+        Assert.True(isValid);
+    }
+    
+    [Theory]
+    [InlineData("XA1234567")]
+    [InlineData("LA123457")]
+    public void PassportShouldNotBeValid(string passportNumber)
+    {
+        // Act
+        var isValid = UserService.ValidatePassportNumber(passportNumber);
+        
+        // Assert
+        Assert.False(isValid);
+    }
+}
--- TravelPort.UnitTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TravelPort.UnitTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
