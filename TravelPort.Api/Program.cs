298c1316f720cc323b7b86ff7cec1f0c275b5f99
Gabriel Masson <157505893+gdmasson@users.noreply.github.com>
2025-11-13 13:37:07 +0100

    setup api interface + docker

--- TravelPort.Api/Program.cs
+++ TravelPort.Api/Program.cs
@@ -1,34 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
+using TravelPort.Api.Models;
+using TravelPort.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddOpenApi(opt => opt.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0);
+builder.Services.AddDbContext<PgContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IPersistenceService, PersistenceService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapOpenApi();
+app.MapScalarApiReference();
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/user/{passportNumber}", async (string passportNumber, IUserService userService) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-        .ToArray();
-    return forecast;
+    var user = await userService.GetUserByPassportNumber(passportNumber);
+    return user != null ? Results.Ok(user) : Results.NotFound();
 });
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+app.MapPost("/user", async ([FromBody] User user, IUserService userService) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var created = await userService.CreateUser(
+        user.PassportNumber,
+        user.SelectedAirport,
+        user.Name,
+        user.Surname,
+        user.Email,
+        null,
+        user.PhoneNumber);
+    return created ? Results.Ok() : Results.BadRequest();
+});
+
+app.MapPut("/user", async ([FromBody] User user, IUserService userService) =>
+{
+    var created = await userService.UpdateUser(
+        user.PassportNumber,
+        user.SelectedAirport,
+        user.Name,
+        user.Surname,
+        user.Email,
+        null,
+        user.PhoneNumber);
+    return created ? Results.Ok() : Results.BadRequest();
+});
+
+app.MapDelete("/user/{passportNumber}", async (string passportNumber, IUserService userService) =>
+{
+    var deleted = await userService.DeleteUser(passportNumber);
+    return deleted ? Results.Ok() : Results.NotFound();
+});
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PgContext>();
+    db.Database.EnsureCreated();
 }
+
+app.Run();
